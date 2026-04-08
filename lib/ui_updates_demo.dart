8e40704a6e22943e21efe1aa24f977b391ea90ba
Paulo <paulotec96@gmail.com>
2026-04-08 12:24:30 -0300

    Refactor UIUpdatesDemo widget from Stateful to Stateless for simplicity

--- lib/ui_updates_demo.dart
+++ lib/ui_updates_demo.dart
@@ -1,23 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_internals/demo_buttons.dart';
 
-class UIUpdatesDemo extends StatefulWidget {
+class UIUpdatesDemo extends StatelessWidget {
   const UIUpdatesDemo({super.key});
-
-  @override
-  StatefulElement createElement() {
-    print('UIUpdatesDemo CREATEELEMENT called');
-    return super.createElement();
-  }
-
-  @override
-  State<UIUpdatesDemo> createState() {
-    return _UIUpdatesDemo();
-  }
-}
-
-class _UIUpdatesDemo extends State<UIUpdatesDemo> {
-  var _isUnderstood = false;
 
   @override
   Widget build(BuildContext context) {
